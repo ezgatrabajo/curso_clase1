1dca86ae96c4a4023b61187cc06666b1223fc588
ezgatrabajo <ezgatrabajo@gmail.com>
2021-01-15 17:27:34 -0300

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
-
+import MarcasForm from "./componentes/MarcasForm";
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Bienvenido</p>
+      <MarcasForm />
     </div>
   );
 }
