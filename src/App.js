8adc936a4239599307104ec1f7250d31a9a9c490
ezgatrabajo <ezgatrabajo@gmail.com>
2021-01-23 13:04:46 -0300

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -55,56 +55,8 @@ export default function App() {
 
   return (
     <div className="container mt-5">
-      <h1 className="text-center">CRUD APP</h1>
+      <h1 className="text-center">PROPS APP</h1>
       <hr />
-      <div className="row">
-        <div className="col-8">
-          <h4 className="text-center">Lista de Tareas</h4>
-          <ul className="list-group">
-            {tareas.map(item => (
-              <li className="list-group-item" key={item.id}>
-                <span className="lead">{item.tarea}</span>
-                <button
-                  className="btn btn-sm btn-danger float-right mx-2"
-                  onClick={() => eliminarTarea(item.id)}
-                >
-                  Eliminar
-                </button>
-                <button
-                  className="btn btn-sm btn-warning float-right"
-                  onClick={() => editar(item)}
-                >
-                  Editar
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="col-4">
-          <h4 className="text-center">
-            {modoEdicion ? "Editar Tarea" : "Agregar Tarea"}
-          </h4>
-          <form onSubmit={modoEdicion ? editarTarea : agregarTarea}>
-            <input
-              type="text"
-              className="form-control mb-2"
-              placeholder="Ingrese Tarea"
-              onChange={e => setTarea(e.target.value)}
-              value={tarea}
-            />
-            {modoEdicion ? (
-              <button className="btn btn-warning btn-block" type="submit">
-                Editar
-              </button>
-            ) : (
-              <button className="btn btn-dark btn-block" type="submit">
-                Agregar
-              </button>
-            )}
-          </form>
-        </div>
-      </div>
     </div>
   );
 }
